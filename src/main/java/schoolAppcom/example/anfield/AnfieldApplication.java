9c2d64d92a3a969750cdc91a2441884b351dc1de
crudeboy <jahswill.apata@gmail.com>
2023-11-01 15:17:50 +0100

    initial push

---
+++ src/main/java/schoolAppcom/example/anfield/AnfieldApplication.java
@@ -0,0 +1,13 @@
+package schoolAppcom.example.anfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnfieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnfieldApplication.class, args);
+	}
+
+}
